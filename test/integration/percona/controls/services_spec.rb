02f9599e371ba76c012f147e94c35854f3de548e
Todd Bryant <todd.bryant@voyager.nz>
2022-08-26 15:46:52 +1200

    style: linting fix

--- test/integration/percona/controls/services_spec.rb
+++ test/integration/percona/controls/services_spec.rb
@@ -4,13 +4,13 @@ control 'mysql@bootstrap service' do
   title 'should be running'
   only_if { sys_info.hostname == 'master-pxc' }
 
-  describe service ('mysql@bootstrap') do
+  describe service('mysql@bootstrap') do
     it { should be_installed }
     it { should_not be_enabled }
     it { should be_running }
   end
 
-  describe service ('mysql') do
+  describe service('mysql') do
     it { should be_installed }
     it { should be_enabled }
     it { should_not be_running }
@@ -21,7 +21,7 @@ control 'mysql service' do
   title 'should be running'
   only_if { sys_info.hostname == 'member-pxc' }
 
-  describe service ('mysql') do
+  describe service('mysql') do
     it { should be_installed }
     it { should be_enabled }
     it { should be_running }
